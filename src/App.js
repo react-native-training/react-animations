9e4bc25672fcd068e978a1b1a564cb5b790daacf
nader dabit <dabit3@gmail.com>
2017-08-05 15:17:09 -0500

    added js-animations example

--- src/App.js
+++ src/App.js
@@ -2,26 +2,64 @@ import React, { Component } from 'react';
 
 class App extends Component {
   state = {
-    focused: false
+    disabled: true,
   }
-  componentDidMount() {
-    this.input.addEventListener('focus', this.focus);
-    this.input.addEventListener('blur', this.focus);
-  }
-  focus = () => {
-    this.setState((state) => ({ focused: !state.focused }))
+  onChange = (e) => {
+    const length = e.target.value.length;
+    if (length >= 4) {
+      this.setState(() => ({ disabled: false }))
+    } else if (!this.state.disabled) {
+      this.setState(() => ({ disabled: true }))
+    }
   }
   render() {
+    const label = this.state.disabled ? 'Disabled' : 'Submit';
     return (
       <div className="App">
-        <div className="container">
+        <div style={styles.container}>
+          <button
+            style={Object.assign({}, styles.button, !this.state.disabled && styles.buttonEnabled)}
+            disabled={this.state.disabled}
+          >{label}</button>
           <input
-            ref={input => this.input = input}
-            className={['input', this.state.focused && 'input-focused'].join(' ')}
+            style={styles.input}
+            onChange={this.onChange}
           />
         </div>
       </div>
     );
+  }
+}
+
+const styles = {
+  container: {
+    width: 500,
+    padding: 50,
+    boxShadow: '1px 1px 14px rgba(0, 0, 0, .4)',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  input: {
+    width: 200,
+    outline: 'none',
+    fontSize: 20,
+    padding: 10,
+    border: 'none',
+    backgroundColor: '#ddd',
+    marginTop: 10,
+  },
+  button: {
+    width: 180,
+    height: 50,
+    border: 'none',
+    borderRadius: 4,
+    fontSize: 20,
+    cursor: 'pointer',
+    transition: '.25s all',
+  },
+  buttonEnabled: {
+    backgroundColor: '#ffc107',
+    width: 220,
   }
 }
 
